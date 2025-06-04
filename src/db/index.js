0c17d3cc4c5a73ab2f8160f552ccb3e056bad418
Pintuknwr <pintuk.ug22.cs@nitp.ac.in>
2025-06-04 16:29:04 +0530

    db setup

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import {DB_NAME} from "../constants.js";
+
+const connectDB =async () =>{
+    try{
+        const dbconnection=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connected: ${dbconnection.connection.host}`);
+
+    }
+    catch(error){
+        console.error("Error connecting to the database: ", error);
+        throw error;
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./config.env"
+});
+
+connectDB();
+
+
+// import express from "express";
+
+// const app= express();
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("Error: ",error)
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`);
+
+//         })
+//     } catch (error) {
+//         console.error("Error ", error);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
